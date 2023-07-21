c806b9e2d293a4d8b447ef8d31ec6d875c27842c
shiggityshaggs <shiggityshaggs@users.noreply.github.com>
2023-07-21 14:15:06 +0200

    Add project files.

---
+++ IServer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+[ComVisible(true)]
+[Guid("60AB5838-13DD-4D5A-A976-A6324CFE0D49")]
+[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+public interface IServer
+{
+    /// <summary>
+    /// Compute the value of the constant Pi.
+    /// </summary>
+    double ComputePi();
+}
